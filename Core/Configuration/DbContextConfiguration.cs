9250da986db8cf455a696394336b6f4ba36b4c90
kamil.oberaj <kamil.oberaj@gmail.com>
2025-01-22 21:40:29 +0100

    adjust for azure

--- Core/Configuration/DbContextConfiguration.cs
+++ Core/Configuration/DbContextConfiguration.cs
@@ -4,9 +4,11 @@ public sealed class DbContextConfiguration(string connectionString)
 {
     private string _connectionString { get; } = connectionString;
 
-    public string ConnectionString => !string.IsNullOrWhiteSpace(_connectionString)
-        ? _connectionString
-        : throw new ArgumentNullException(nameof(_connectionString));
+    public string ConnectionString => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("HpgwartsDatabase"))
+        ? Environment.GetEnvironmentVariable("HpgwartsDatabase")!
+        : !string.IsNullOrWhiteSpace(_connectionString)
+            ? _connectionString
+            : throw new ArgumentNullException(nameof(_connectionString));
     
     public const string SectionName = "Database";
 }
